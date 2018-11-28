6d170ec9cb548791640840663ea8ad4f035e1097
NicolaiV <nicolai.verbaarschot@gmail.com>
2018-11-28 19:19:54 +0100

    Updated final assembly code - tested on codejudge

--- popRecursion.asm
+++ popRecursion.asm
@@ -1,8 +1,15 @@
-    @10     // set n 
-    D=A
+    @R0     // Fetch n from R0 and store in D
+    D=M
+
+    @NISZERO// Check if n=0 (edge case 1/2)
+    D;JEQ
+
+    @NISONE // Check if n=1 (edge case 2/2)
+    D=D-1
+    D;JEQ
+
 
     @R0
-    M=D     // R0=n=10
     M=M-1   // n-1
     M=M-1   // n-1 // Once for formula, twice for correcting loop
    
@@ -26,5 +33,21 @@
     0;JMP
     
 (END)
+    @END
+    0;JMP
+
+
+
+(NISZERO)   // Edge case 1/2: R1=0
+    @R1
+    M=0
+
+    @END
+    0;JMP
+
+(NISONE)    // Edge case 2/2: R1=1
+    @R1
+    M=1
+
     @END
     0;JMP
